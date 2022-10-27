c51392bb03be32f745f75c342265448f98582be8
cpkopp <cameronpkopp@gmail.com>
2022-10-26 19:56:55 -1000

    started routes in category-routes, next keep adding the routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,16 +1,65 @@
+//DECLARATIONS: router, category and product models -----------------------
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
-// The `/api/categories` endpoint
-
+//ROUTES: /api/categories ---------------------------------------
+// get all categories
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    //category table columns
+    attributes: [
+      'id',
+      'category_name'
+    ],
+    //product table columns
+    include: [
+      {
+        model: Product,
+        attributes: [
+          'id',
+          'product_name'
+        ]
+      }
+    ]
+  }).then(dbPostData => res.json(dbPostData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
+// get one category by id
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    //category table columns
+    attributes: [
+      'id',
+      'category_name'
+    ],
+    //product table columns
+    include: [
+      {
+        model: Product,
+        attributes: [
+          'id',
+          'product_name',
+          'price',
+          'stock'
+        ]
+      }
+    ]
+  }).then(dbPostData => res.json(dbPostData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.post('/', (req, res) => {
