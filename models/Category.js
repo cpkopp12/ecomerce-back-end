97ed23b4db14fa6dad146c997a50f02df4f60e63
cpkopp <cameronpkopp@gmail.com>
2022-10-26 17:12:32 -1000

    models set up, next set up relations in index

--- models/Category.js
+++ models/Category.js
@@ -1,12 +1,23 @@
+//DECLARATIONS: sequelize, db connection -----------------------------
 const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
+//CATEGORY MODEL --------------------------------
 class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
@@ -17,4 +28,5 @@ Category.init(
   }
 );
 
+//EXPORT MODEL --------------------------
 module.exports = Category;
